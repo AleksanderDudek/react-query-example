8e30a3e7c0ab3232f2d622f9f417d9ee7078d91d
Aleksander Dudek <aleksander.dudek@rallyhealth.com>
2023-05-02 20:37:31 +0200

    Created initial setup + installed packages

---
+++ src/pages/PostLists.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddPost from '../components/AddPost';
+
+const PostLists = () => {
+    return (
+        <div>
+            <h1>PostLists</h1>
+            <AddPost />
+        </div>
+    )
+}
+
+export default PostLists;
